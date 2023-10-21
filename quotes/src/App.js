26a814a8c49b4dec988941c675193d4cca5dbbd5
Anish <xevenbiswas@gmail.com>
2023-10-21 15:19:10 +0530

    fixed

--- quotes/src/App.js
+++ quotes/src/App.js
@@ -4,7 +4,9 @@ import RandomQuote from "./Components/RandomQuote/RandomQuote"
 function App() {
   return (
     <div className="App">
+      <header className="App-header">Neumorphism Quotes</header>
       <RandomQuote />
+      <div className="foot">Made by <a href="https://bento.me/anish7">Anish</a></div>
     </div>
   );
 }
--- quotes/src/Components/RandomQuote/RandomQuote.jsx
+++ quotes/src/Components/RandomQuote/RandomQuote.jsx
@@ -3,40 +3,46 @@ import xSvg from "../Assets/x.svg";
 import "./Rq.css";
 
 const RandomQuote = () => {
-
-  let quotes=[];
+  let quotes = [];
   async function loadQuotes() {
     const response = await fetch("https://type.fit/api/quotes");
     const data = await response.json();
-    quotes=data;
+    quotes = data;
     console.log(quotes);
   }
   const randomQuote = () => {
     let index = Math.floor(Math.random() * quotes.length);
     let quote = quotes[index];
     setQuote(quote);
-  }
+  };
 
   const [quote, setQuote] = useState({
     text: "Need new haters, old ones became my fans",
     author: "Anish",
   });
-loadQuotes();
+
+  const twitter=()=>{
+    window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(',')[0]}`)
+  }
+  loadQuotes();
   return (
     <div className="container">
       <div className="text">{quote.text}</div>
-      <div className="lower"> 
+      <div className="lower">
         <div className="line"></div>
         <div className="share">
-          <div className="author"> -  {quote.author}</div>
+          <div className="author"> - {quote.author.split(',')[0]}</div>
           <div className="icons">
             <img
-              width="50"
-              height="50"
               src="https://img.icons8.com/ios/50/available-updates.png"
-              alt="reload" onClick={() => {randomQuote()}}
+              alt="reload"
+              onClick={() => {
+                randomQuote();
+              }}
             />
-            <img src={xSvg} alt="X" />
+            <img src={xSvg} alt="X" onClick={() => {
+                twitter();
+              }}/>
           </div>
         </div>
       </div>
